dee69ac0f71ce5691552421e88c0f91fa6e315f1
King Songtao <david.songtao@hotmail.com>
2024-05-14 21:17:15 +0100

    test python env

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+This is a testing file to test Linear Algebra Learning
+"""
+import sys
+import numpy
+import scipy
+
+if __name__ == "__main__":
+    print(numpy.__version__)
+    print(scipy.__version__)
+    print(sys.version)
