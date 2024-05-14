e4cbca43778b2a48a272805e6b7d74f809d01cd8
King Songtao <david.songtao@hotmail.com>
2024-05-14 23:41:27 +0100

    测试向来运算

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-"""
-This is a testing file
-"""
+from playLA.Vector import Vector
+
+#
+if __name__ == "__main__":
+    vec = Vector([5, 2])
+
+    vec2 = Vector([1, 2])
+    result = vec + vec2
+    print(result)
+
+    result2 = 3 * vec2
+    print(result2)
+
+    result3 = vec2 - vec
+    print(result3)
+
+    result4 = -vec
+    print(result4)
