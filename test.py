6a7d793d6d76fa1224082871ec4c69133f9cbcd2
King Songtao <david.songtao@hotmail.com>
2024-05-16 13:13:01 +0100

    这是一个测试文件

--- test.py
+++ test.py
@@ -16,3 +16,29 @@ if __name__ == "__main__":
 
     result4 = -vec
     print(result4)
+
+    zer0_vec = Vector.zero(4)
+    print(zer0_vec)
+
+    result5 = vec * 0
+    print(result5)
+
+    result6 = vec.norm()
+    print(result6)
+
+    try:
+        print(zer0_vec.normalize())
+    except ZeroDivisionError:
+        print(f"can not divide by zero: Vector{zer0_vec}")
+
+    s1 = Vector([2, 5])
+    s2 = Vector([5, 2])
+
+    print(s1.dot(s2))
+
+    point = s1.get_pro_point(s2)
+    print(f"{s1}向量在{s2}向量上的投影点的坐标是：{point}")
+
+
+
+
