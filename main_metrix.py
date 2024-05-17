695f652cd00f6a953b387759c0eee6f989340454
King Songtao <david.songtao@hotmail.com>
2024-05-17 13:43:43 +0100

    更新了矩阵和向量的乘法操作

--- main_metrix.py
+++ main_metrix.py
@@ -1,4 +1,5 @@
 from playLA.Metrix import Metrix
+from playLA.Vector import Vector
 
 
 if __name__ == '__main__':
@@ -12,6 +13,8 @@ if __name__ == '__main__':
         [7, 8]
     ])
 
+    vec = Vector([1, 2])
+
     print(metrix)
 
     print(f"{metrix}矩阵中的第二行第一列是：{metrix[1,0]}")
@@ -23,3 +26,5 @@ if __name__ == '__main__':
     print(f"3乘以{metrix}矩阵的结果是：{3 * metrix}")
     print(f"{metrix}矩阵除以3的结果是：{metrix / 2}")
     print(f"创建一个有两行三列的零矩阵：{Metrix.zero(2,3)}")
+    print(f"{metrix}矩阵与{vec}向量相乘的结果是{metrix.metrix_mul(vec)}")
+
