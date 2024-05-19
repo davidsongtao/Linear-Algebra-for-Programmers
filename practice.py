b14d511c3c9ac0ea168122ee1d8f980da45b090e
King Songtao <david.songtao@hotmail.com>
2024-05-19 01:47:36 +0100

    封装了图形按倍数放大或缩小的方法

---
+++ practice.py
@@ -0,0 +1,70 @@
+"""
+实现输入自己的图形变换库：
+    - 用点绘制出一个长方形或圆形
+    - 定义变换矩阵，使用矩阵变换方法变换图形
+    - 利用矩阵形成自己的图形变换库
+"""
+import matplotlib.pyplot as plt
+from playLA.Metrix import Metrix
+from playLA.Vector import Vector
+
+
+# DONE 实现不同的变换矩阵，从而实现不同的图形变换方法
+
+def zoom(points, zoom_x, zoom_y):
+    """
+    本方法实现了将图形按照参数放大或缩小
+    :param points: 要缩放的图形坐标列表（二元列表）
+    :param zoom_x: 各个坐标X轴放大的倍数
+    :param zoom_y: 各个坐标Y轴放大的倍数
+    :return: 返回缩放后的坐标
+    """
+    # 把传入的点坐标(二维列表)实例化成一个矩阵对象P
+    p = Metrix(points)
+    # 定义一个变换矩阵T，实现将x轴放大x倍，y轴放大y倍
+    assert zoom_x > 0 and zoom_y > 0, \
+        "Zoom parameters must be greater than 0"
+    T = Metrix([[zoom_x, 0], [0, zoom_y]])
+    # 变换矩阵T点乘坐标矩阵P，获得zoom后的矩阵
+    p2 = T.dot(p.T())
+    # 获取缩放后的所有X值和所有Y值并组成新矩阵
+    x = [p2.col_vector(i)[0] -
+         (points[0][0] * zoom_x - points[0][0]) for i in range(p2.col_num())]
+    y = [p2.col_vector(i)[1] -
+         (points[0][1] * zoom_y - points[0][1]) for i in range(p2.col_num())]
+    # 将更新后的坐标重新组织并返回
+    return [list(pair) for pair in zip(x, y)]
+
+
+if __name__ == '__main__':
+    rectangle_points = [
+        [0, 0],
+        [0, 5],
+        [3, 5],
+        [3, 4],
+        [1, 4],
+        [1, 3],
+        [2, 3],
+        [2, 2],
+        [1, 2],
+        [1, 0]
+    ]
+
+    # DONE 绘制一个原图形
+    x = [points[0] for points in rectangle_points]
+    y = [points[1] for points in rectangle_points]
+
+    plt.figure(figsize=(5, 5))
+    plt.xlim(-15, 15)
+    plt.ylim(-15, 15)
+    plt.plot(x, y)
+    # plt.show()
+
+    # DONE 测试长方形的放大缩小
+    updated_points = zoom(rectangle_points, 2, 1.5)
+
+    x_new = [points[0] for points in updated_points]
+    y_new = [points[1] for points in updated_points]
+
+    plt.plot(x_new, y_new)
+    plt.show()
